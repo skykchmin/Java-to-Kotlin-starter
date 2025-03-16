caf791e33b0940070e75150dfb7089b69398b7b7
skykchmin <skykchmin@naver.com>
2025-03-16 14:29:56 +0900

    [250315] feat: 연산자 처리

---
+++ src/main/java/com/lannstark/lec04/Lec04MainKtV1.kt
@@ -0,0 +1,10 @@
+package com.lannstark.lec04
+
+fun main(){
+    val money1 = JavaMoney(1_000L)
+    val money2 = money1
+    val money3 = JavaMoney(1_000L)
+
+    println(money1 == money3)
+    println(money1 === money2)
+}
